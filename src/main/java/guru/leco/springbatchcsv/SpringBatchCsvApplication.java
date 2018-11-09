0a759b6a6edf910c33cf18197a77d85a15c5e3ec
leco <lcosta.sp.br@gmail.com>
2018-11-08 22:51:57 -0200

    first commit

---
+++ src/main/java/guru/leco/springbatchcsv/SpringBatchCsvApplication.java
@@ -0,0 +1,41 @@
+package guru.leco.springbatchcsv;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+@SpringBootApplication
+public class SpringBatchCsvApplication {
+
+    public static void main(String[] args) {
+        String[] springConfig =
+                {
+                        "spring/batch/config/database.xml",
+                        "spring/batch/config/context.xml",
+                        "spring/batch/jobs/job-report.xml"
+                };
+
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext(springConfig);
+
+        JobLauncher jobLauncher = (JobLauncher) context.getBean("jobLauncher");
+        Job job = (Job) context.getBean("reportJob");
+
+        try {
+
+            JobExecution execution = jobLauncher.run(job, new JobParameters());
+            System.out.println("Exit Status : " + execution.getStatus());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Done");
+    }
+}
---
+++ src/main/java/guru/leco/springbatchcsv/model/Report.java
@@ -0,0 +1,14 @@
+package guru.leco.springbatchcsv.model;
+
+import lombok.Data;
+
+@Data
+public class Report {
+
+    private String Date;
+    private String Impressions;
+    private String Clicks;
+    private String Earning;
+
+
+}
